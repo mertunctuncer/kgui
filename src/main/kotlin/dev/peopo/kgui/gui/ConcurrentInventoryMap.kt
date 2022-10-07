f63b519d6f07f8a73cdef955e54912227a5b12cb
mertunctuncer <mertunctuncer@gmail.com>
2022-10-07 18:15:37 +0300

    Added concurrent lazy inventory caching.

---
+++ src/main/kotlin/dev/peopo/kgui/gui/ConcurrentInventoryMap.kt
@@ -0,0 +1,17 @@
+package dev.peopo.kgui.gui
+
+import org.bukkit.inventory.Inventory
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+internal class ConcurrentInventoryMap(
+	private val baseInventory: Inventory,
+	private val title: String,
+	private val valueCache: ConcurrentHashMap<UUID, GUIHolder> = ConcurrentHashMap()
+) : Map<UUID, GUIHolder> by valueCache {
+	override operator fun get(key: UUID): GUIHolder = valueCache[key] ?: let {
+		val holder = GUIHolder(baseInventory, title)
+		valueCache[key] = holder
+		return@let holder
+	}
+}
